50145b68ac619ccd97994954832ffc53bb04a4bc
yh_habosol <k0912h@naver.com>
2020-03-10 18:07:10 +0900

    weather part 2

--- weather.js
+++ weather.js
@@ -1,12 +1,22 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "6a5a5a4d708e85e2f35ddca0a1e7bbb1";
 const COORDS = "coords";
-
+/////// 이 부분은 풀스텍 강좌 들어야 이해 할 수 있는 부분////////////////////////////////////////////////////////////
 function getWeather(lat, lng) {
   fetch(
-    `http://history.openweathermap.org/data/2.5/history/city?lat=${lat}&lon=${lng}&type=hour&start={start}&end={end}&appid={API_KEY}`
-  );
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature}'C, ${place}`;
+    });
 }
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
