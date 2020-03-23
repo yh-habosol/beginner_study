c72414627a2c6eaa5df70d78249cee48e4266c46
yh_habosol <k0912h@naver.com>
2020-03-23 21:15:08 +0900

    handling route with express

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 
-const PORT = 4000;
+const PORT = 5000;
 
 function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}`);
 }
+
+function handleHome(req, res) {
+  console.log(req);
+  res.send("Hello from home");
+}
+
+function handleProfile(req, res) {
+  res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
