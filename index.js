549b9b9798eae9bea7547c7d0bbdabe7b7ae520a
yh_habosol <k0912h@naver.com>
2020-03-25 11:26:36 +0900

    Middlewares part two

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import podyParser from "body-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 5000;
@@ -10,12 +16,11 @@ const handleHome = (req, res) => res.send("Hello from aasdafsdf");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenHome = (req, res, next) => {
-  console.log("Between");
-  next();
-};
-
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+app.use(helmet());
 
 app.get("/", handleHome);
 
