b70798eb685990625f79cf0b7bfbc287bcd929d7
yh_habosol <k0912h@naver.com>
2020-03-27 10:33:27 +0900

    Layout with Pug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Login");
-export const logout = (req, res) => res.send("Logout");
-export const users = (req, res) => res.send("Users");
-export const userDetail = (req, res) => res.send("UserDetail");
-export const editProfile = (req, res) => res.send("EditProfile");
-export const changePassword = (req, res) => res.send("ChangePassword");
+export const join = (req, res) => res.render("Join");
+export const login = (req, res) => res.render("Login");
+export const logout = (req, res) => res.render("Logout");
+export const users = (req, res) => res.render("Users");
+export const userDetail = (req, res) => res.render("UserDetail");
+export const editProfile = (req, res) => res.render("EditProfile");
+export const changePassword = (req, res) => res.render("ChangePassword");
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
 export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("Videos");
-export const upload = (req, res) => res.send("Upload");
-export const videoDetail = (req, res) => res.send("VideoDetail");
-export const editVideo = (req, res) => res.send("EditVideo");
-export const deleteVideo = (req, res) => res.send("DeleteVideo");
+export const search = (req, res) => res.render("Search");
+export const videos = (req, res) => res.render("Videos");
+export const upload = (req, res) => res.render("Upload");
+export const videoDetail = (req, res) => res.render("VideoDetail");
+export const editVideo = (req, res) => res.render("EditVideo");
+export const deleteVideo = (req, res) => res.render("DeleteVideo");
