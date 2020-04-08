5e65fbef569a1f5eadef8017f9a76c004ae75acc
yh_habosol <k0912h@naver.com>
2020-04-08 19:11:06 +0900

    Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,82 +1,14 @@
-export const videos = [
-  {
-    id: 324393,
+import mongoose from "mongoose";
 
-    title: "Video awesome",
+mongoose.connect("mongodb://localhost:27017/wetube", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
 
-    description: "This is something I love",
+const db = mongoose.connection;
 
-    views: 24,
+const handleOpen = () => console.log("Connected to DB");
+const handleError = (error) => console.log(`Error on DB Connection:${error}`);
 
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-
-    creator: {
-      id: 35634563456345,
-
-      name: "Nicolas",
-
-      email: "nico@las.com",
-    },
-  },
-  {
-    id: 768684,
-
-    title: "Video super",
-
-    description: "This is something I love",
-
-    views: 24,
-
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-
-    creator: {
-      id: 56565757,
-
-      name: "Nicolas",
-
-      email: "nico@las.com",
-    },
-  },
-  {
-    id: 343434,
-
-    title: "Video prefect",
-
-    description: "This is something I love",
-
-    views: 24,
-
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-
-    creator: {
-      id: 121212,
-
-      name: "Nicolas",
-
-      email: "nico@las.com",
-    },
-  },
-  {
-    id: 123,
-
-    title: "Video nice",
-
-    description: "This is something I love",
-
-    views: 24,
-
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-
-    creator: {
-      id: 121212,
-
-      name: "Nicolas",
-
-      email: "nico@las.com",
-    },
-  },
-];
+db.once("open", handleOpen);
+db.on("error", handleError);
