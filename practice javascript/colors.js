175432ba5b5bd477296262a24936eeeb46ed53c5
yh-habosol <k0912h@naver.com>
2020-05-06 10:18:23 +0900

    use jQuery

--- practice javascript/colors.js
+++ practice javascript/colors.js
@@ -1,20 +1,23 @@
 let Links = {
   setColor: function (color) {
-    const alist = document.querySelectorAll("a");
-    let i = 0;
-    while (i < alist.length) {
-      alist[i].style.color = color;
-      i = i + 1;
-    }
+    // const alist = document.querySelectorAll("a");
+    // let i = 0;
+    // while (i < alist.length) {
+    //   alist[i].style.color = color;
+    //   i = i + 1;
+    // }
+    $("a").css("color", color);
   },
 };
 
 let Body = {
   setBackgroundColor: function (color) {
-    document.querySelector("body").style.backgroundColor = color;
+    //document.querySelector("body").style.backgroundColor = color;
+    $("body").css("backgroundColor", color);
   },
   setColor: function (color) {
-    document.querySelector("body").style.color = color;
+    //document.querySelector("body").style.color = color;
+    $("body").css("color", color);
   },
 };
 
