37a933463c6f55f032373ea9639bd886b518eeb7
yh-habosol <k0912h@naver.com>
2020-05-06 10:07:25 +0900

    divide javascript file

---
+++ practice javascript/colors.js
@@ -0,0 +1,35 @@
+let Links = {
+  setColor: function (color) {
+    const alist = document.querySelectorAll("a");
+    let i = 0;
+    while (i < alist.length) {
+      alist[i].style.color = color;
+      i = i + 1;
+    }
+  },
+};
+
+let Body = {
+  setBackgroundColor: function (color) {
+    document.querySelector("body").style.backgroundColor = color;
+  },
+  setColor: function (color) {
+    document.querySelector("body").style.color = color;
+  },
+};
+
+function nightDayHandler(self) {
+  if (self.value === "night") {
+    Body.setBackgroundColor("black");
+    Body.setColor("white");
+    self.value = "day";
+
+    Links.setColor("powderblue");
+  } else {
+    Body.setBackgroundColor("white");
+    Body.setColor("black");
+    self.value = "night";
+
+    Links.setColor("blue");
+  }
+}
