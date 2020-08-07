b28e59ee8a4f239a93db13accc8412760624505f
yh-habosol <k0912h@naver.com>
2020-08-07 18:55:59 +0900

    add middleware

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
@@ -11,6 +11,13 @@ const handleHome = (req, res) => {
   console.log("Hi from Home");
   res.send("Hi");
 };
+
+const between = (req, res, next) => {
+  console.log("between");
+  next();
+};
+
+app.use(between);
 
 app.get("/", handleHome);
 
