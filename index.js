8f24da8889ae95d526a0d49db8ebd87eb5f81401
yh-habosol <k0912h@naver.com>
2020-08-07 17:12:23 +0900

    Use Router

--- index.js
+++ index.js
@@ -7,4 +7,11 @@ const handleListening = () => {
   console.log(`Listening on: http://localhost:${PORT}`);
 };
 
+const handleHome = (req, res) => {
+  console.log("Hi from Home");
+  res.send("Hi");
+};
+
+app.get("/", handleHome);
+
 app.listen(PORT, handleListening);
