e3c5895d65d49dfce493897ced90a48c6411ab37
yh-habosol <k0912h@naver.com>
2020-08-07 19:22:32 +0900

    add middleware

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -9,16 +14,19 @@ const handleListening = () => {
 
 const handleHome = (req, res) => {
   console.log("Hi from Home");
-  res.send("Hi");
+  res.send("Home");
+};
+const handleProfile = (req, res) => {
+  console.log("Hi from Profile");
+  res.send("Profile");
 };
 
-const between = (req, res, next) => {
-  console.log("between");
-  next();
-};
-
-app.use(between);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("tiny"));
 
 app.get("/", handleHome);
-
+app.get("/profile", handleProfile);
 app.listen(PORT, handleListening);
