e9c2ec27366a427618f3e1d65d9b8ef06049887f
yh-habosol <k0912h@naver.com>
2020-08-09 16:12:37 +0900

    add controller
    make MVC pattern

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,18 @@
 import express from "express";
 import routes from "../routes";
+import {
+  videos,
+  upload,
+  editVideo,
+  deleteVideo,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.home, (req, res) => res.send("videos"));
-videoRouter.get(routes.upload, (req, res) => res.send("upload"));
+videoRouter.get(routes.home, videos);
+videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.videoDetail, (req, res) => res.send("videoDetail"));
-videoRouter.get(routes.editVideo, (req, res) => res.send("editVideo"));
-videoRouter.get(routes.deleteVideo, (req, res) => res.send("deleteVideo"));
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
