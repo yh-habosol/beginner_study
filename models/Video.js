f0699dcbba5d5a8d2b98b8e51eb3aabb27b293e8
yh-habosol <k0912h@naver.com>
2020-08-15 12:19:26 +0900

    Video Model

---
+++ models/Video.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const VideoSchema = new mongoose.Schema({
+  fileUrl: {
+    type: String,
+    required: "File URL is required",
+  },
+  title: {
+    type: String,
+    required: "Title is required",
+  },
+  description: String,
+  views: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.OpjectId,
+      ref: "Comment",
+    },
+  ],
+});
+
+const model = mongoose.Schema.model("Video", VideoSchema);
+export default model;
