5f89297eca5af4eb9613527cf0a6a268c553bd16
yh-habosol <k0912h@naver.com>
2020-08-15 11:58:34 +0900

    use dotenv

--- init.js
+++ init.js
@@ -1,7 +1,9 @@
 import app from "./app";
 import "./db";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 
 const handleListening = () => {
   console.log(`Listening on: http://localhost${PORT}`);
