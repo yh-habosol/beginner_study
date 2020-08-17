d544736ad86fedd4f242e7aae54619525d2ef889
yh-habosol <k0912h@naver.com>
2020-08-17 17:43:08 +0900

    Searching Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -10,10 +10,19 @@ export const home = async (req, res) => {
     res.render("home", { pageTitle: "Home", videos });
   }
 };
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
+
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" },
+    });
+  } catch (error) {
+    console.log(error);
+  }
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
