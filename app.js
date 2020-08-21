97b0da44993ab6416a8788acbb610b03562f9ae2
yh-habosol <k0912h@naver.com>
2020-08-21 17:30:19 +0900

    User Login

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 import { localsMiddleware } from "./middlewares";
+import passport from "passport";
+import "./passport";
 
 const app = express();
 
@@ -18,7 +20,9 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
-app.use(morgan("tiny"));
+app.use(morgan("dev"));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(localsMiddleware);
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,12 @@
 import routes from "../routes";
 import User from "../models/User";
+import passport from "passport";
 
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = async (req, res) => {
+export const postJoin = async (req, res, next) => {
   const {
     body: { name, email, password, password2 },
   } = req;
@@ -20,8 +21,10 @@ export const postJoin = async (req, res) => {
         email,
       });
       await User.register(user, password);
+      next();
     } catch (error) {
       console.log(error);
+      res.redirect(routes.home);
     }
   }
 };
@@ -29,9 +32,11 @@ export const postJoin = async (req, res) => {
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Login" });
 
-export const postLogin = (req, res) => {
-  res.redirect(routes.home);
-};
+export const postLogin = (req, res) =>
+  passport.authenticate("local", {
+    failureRedirect: routes.login,
+    successRedirect: routes.home,
+  });
 
 export const logout = (req, res) => res.redirect(routes.home);
 
