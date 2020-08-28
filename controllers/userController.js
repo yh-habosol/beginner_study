db06706ee688a8bbdadf55aeb50d2b542b452191
yh-habosol <k0912h@naver.com>
2020-08-28 16:03:35 +0900

    Change Password

--- controllers/userController.js
+++ controllers/userController.js
@@ -103,9 +103,28 @@ export const postEditProfile = async (req, res) => {
     });
     res.rediret(routes.me);
   } catch (error) {
-    res.render("editProfile", { pageTitle: "Edit Prifile" });
+    res.redirect(routes.editProfile);
   }
 };
 
-export const changePassword = (req, res) =>
+export const getChangePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "ChangePassword" });
+
+export const postChangePassword = async (req, res) => {
+  const {
+    body: { oldPassword, newPassword, newPassword2 },
+  } = req;
+
+  try {
+    if (newPassword !== newPassword2) {
+      res.status(400);
+      res.redirect(`/users/${routes.changePassword}`);
+      return;
+    }
+    await req.user.changePassword(oldPassword, newPassword);
+    res.redirect(routes.me);
+  } catch (error) {
+    res.status(400);
+    res.redirect(`/users/${routes.changePassword}`);
+  }
+};
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -6,6 +6,8 @@ import {
   changePassword,
   getEditProfile,
   postEditProfile,
+  getChangePassword,
+  postChangePassword,
 } from "../controllers/userController";
 import { onlyPublic, onlyPrivate, uploadAvatar } from "../middlewares";
 
@@ -14,6 +16,8 @@ const userRouter = express.Router();
 userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
 
-userRouter.get(routes.changePassword, onlyPrivate, changePassword);
+userRouter.get(routes.changePassword, onlyPrivate, getChangePassword);
+userRouter.post(routes.changePassword, onlyPrivate, postChangePassword);
+
 userRouter.get(routes.userDetail(), userDetail);
 export default userRouter;
