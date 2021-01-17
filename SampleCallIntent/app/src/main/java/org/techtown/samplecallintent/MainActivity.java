4f8514b96e928a4f6f4031fc708138c3c6e6b31d
yh-habosol <k0912h@naver.com>
2021-01-17 16:17:10 +0900

    Intent

---
+++ SampleCallIntent/app/src/main/java/org/techtown/samplecallintent/MainActivity.java
@@ -0,0 +1,43 @@
+package org.techtown.samplecallintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = editText.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(data));
+                startActivity(intent);
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                ComponentName name = new ComponentName("org.techtown.samplecallintent", "org.techtown.samplecallintent.MenuActivity");
+                intent.setComponent(name);
+                startActivityForResult(intent, 101);
+            }
+        });
+
+    }
+}
