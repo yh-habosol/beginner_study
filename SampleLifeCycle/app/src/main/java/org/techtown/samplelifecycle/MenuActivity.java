47d13fc16a66b0f858329ec71e96d37fbca138ae
yh-habosol <k0912h@naver.com>
2021-01-18 21:06:50 +0900

    Life cycle

---
+++ SampleLifeCycle/app/src/main/java/org/techtown/samplelifecycle/MenuActivity.java
@@ -0,0 +1,31 @@
+package org.techtown.samplelifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+
+
+}
